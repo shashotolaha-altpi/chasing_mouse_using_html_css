15964c1dcabd57dce2a5fe2f8359e0df83e20e30
shashoto <shashotolaha.altpi@gmail.com>
2023-04-12 17:04:40 +0530

    add some background images in code. fixed bug(mouse can be hover at edge). made the mouse start from middle

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const maxWidth = window.innerWidth - 100;
-const maxHeight = window.innerHeight - 100;
-console.log(`Maximum width: ${maxWidth}`);
-console.log(`Maximum height: ${maxHeight}`);
+const maxWidth = window.innerWidth / 2 - 100;
+const maxHeight = window.innerHeight / 2 - 100;
+// console.log(`Maximum width: ${maxWidth}`);
+// console.log(`Maximum height: ${maxHeight}`);
 function mousehover() {
   let icon = document.getElementById("icon");
+  let out = document.getElementById("outter");
 
   //   const min = 1;
   //   const max = 14;
-  const min = 100;
+  const minw = -maxWidth;
   const maxw = maxWidth;
+  const minh = -maxHeight;
   const maxh = maxHeight ;
-  const numx = Math.floor(((Math.random() * (maxw - min + 1)) + min));
-  const numy = Math.floor(((Math.random() * (maxh - min + 1)) + min));
-  icon.style.transform = `translate(${numx}px,${numy}px)`   ;
-    console.log("x ", numx);
-    console.log("y ", numy);
+  const numx = Math.floor(((Math.random() * (maxw - minw + 1)) + minw));
+  const numy = Math.floor(((Math.random() * (maxh - minh + 1)) + minh));
+  // icon.style.transform = `translate(${numx}px,${numy}px)` ;
+  out.style.transform = `translate(${numx}px,${numy}px)`;
+    // console.log("x ", numx);
+    // console.log("y ", numy);
   //   console.log("hover");
 }
 
